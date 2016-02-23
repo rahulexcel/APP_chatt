03188ded85081dc969e5e7bf636fa4154f81a0fc
rahulexcel <rahul.excel2011@gmail.com>
2016-02-23 12:13:17 +0530

    clearing master

--- design/www/app/chatt.constant.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    'use strict';
-    angular.module('starter')
-        .constant('Configurations', {
-            Hostserver: 'http://192.168.1.149:3000/api/users',
-            senderID: '117380048302',
-            icon: 'chatt'
-        });
-})();
