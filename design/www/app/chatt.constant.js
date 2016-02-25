82b57b9bf64db9166d5f4323f9aeea5c00c269bc
manishiitg <manish@excellencetechnologies.in>
2016-02-25 13:39:42 +0530

    committed

--- design/www/app/chatt.constant.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    'use strict';
-    angular.module('starter')
-        .constant('Configurations', {
-            api_url: 'http://144.76.34.244:3033/api',
-            senderID: '117380048302',
-            icon: 'chatt'
-        });
-})();
--- design/www/app/login/login.factory.js
+++
@@ -1,9 +0,0 @@
-(function() {
-   'use strict';
-   angular.module('starter')
-       .factory('loginFactory', loginFactory);
-
-   function loginFactory($resource, Configurations) {
-       return $resource(Configurations.api_url+'/users/register_login/:action_type/:action/:social_id/:platform/:device_id/:token/:email/:password', {},{});
-   };
-})();
