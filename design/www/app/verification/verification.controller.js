9523d3fe47d7a2b6cbd29593fd20f54aae58a60f
rahulexcel <rahul.excel2011@gmail.com>
2016-02-25 12:18:31 +0530

    Made changs as per pull request commit.

--- design/www/app/verification/verification.controller.js
+++ design/www/app/verification/verification.controller.js
@@ -1,38 +1,35 @@
  (function() {
-    'use strict';
+     'use strict';
 
-    angular.module('starter')
-        .controller('verificationController', verificationController);
+     angular.module('starter')
+         .controller('verificationController', verificationController);
 
-    function verificationController($scope, $state, verificationFactory, tostService, timeStorage) {
-            console.log('verificationController');
-            this.data={
-                varificationCode:'',
-                email:''
-                }
-                if(timeStorage.get('userEmail')){
-                 this.data.email = timeStorage.get('userEmail');
-                }
-            this.verify = function(){
-                if(_.isEmpty(this.data.email) || _.isEmpty(this.data.varificationCode)){
-                    tostService.notify('Please all fill the fields.', 'top');
-                } else{
-                    var query = verificationFactory.save({
-                    email: this.data.email,
-                    code: this.data.varificationCode
-                    });
-                    query.$promise.then(function(data) {
-                        console.log(data);
-                        tostService.notify(data.message, 'top');
-                        if(data.status == 1){
-                            $state.go('login');
-                        }
-                    });
-                }
-            };
-           if(timeStorage.get('fromLoginPage')){
-               console.log('fromLoginPage');
-               this.showEmailInput = true;
-           }
-    }
-})();
+     function verificationController($state, verificationFactory, tostService, timeStorage) {
+         this.data = {
+             verificationCode: '',
+             email: ''
+         }
+         if (timeStorage.get('userEmail')) {
+             this.data.email = timeStorage.get('userEmail');
+         }
+         this.verify = function() {
+             if (_.isEmpty(this.data.email) || _.isEmpty(this.data.verificationCode)) {
+                 tostService.notify('Please all fill the fields.', 'top');
+             } else {
+                 var query = verificationFactory.save({
+                     email: this.data.email,
+                     code: this.data.verificationCode
+                 });
+                 query.$promise.then(function(data) {
+                     tostService.notify(data.message, 'top');
+                     if (data.status == 1) {
+                         $state.go('login');
+                     }
+                 });
+             }
+         };
+         if (timeStorage.get('fromLoginPage')) {
+             this.showEmailInput = true;
+         }
+     }
+ })();
