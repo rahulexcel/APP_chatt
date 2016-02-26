a13f2b4cf07f41f0c9fa7b0e9fbcc3f1d7e7e6ff
rahulexcel <rahul.excel2011@gmail.com>
2016-02-26 11:24:24 +0530

    Done modification as per pull request comment

--- design/www/app/forgotpassword/forgotpassword.controller.js
+++ design/www/app/forgotpassword/forgotpassword.controller.js
@@ -1,32 +1,32 @@
  (function() {
-    'use strict';
+     'use strict';
 
-    angular.module('starter')
-        .controller('forgotPasswordController', forgotPasswordController);
+     angular.module('starter')
+         .controller('forgotPasswordController', forgotPasswordController);
 
-    function forgotPasswordController($scope, $state, forgotPasswordFactory, tostService) {
-            console.log('forgotPasswordController');
-            var self = this;
-            self.data={
-                email:''
-                }
-            self.forgotPassword = function(){
-                if (_.isEmpty(self.data.email)) {
-                    tostService.notify('Please enter your correct email', 'top');
-                } else{
-                    self.forgotSpinner = true;
-                    var query = forgotPasswordFactory.save({
-                    email: self.data.email
-                    });
-                    query.$promise.then(function(data) {
-                        console.log(data);
-                        self.forgotSpinner = false;
-                        tostService.notify(data.message, 'top');
-                        if(data.status == 1){
-                           $state.go('login'); 
-                        }
-                    });
-                }
-            };
-    }
-})();
+     function forgotPasswordController($state, forgotPasswordFactory, tostService) {
+         console.log('forgotPasswordController');
+         var self = this;
+         self.data = {
+             email: ''
+         }
+         self.forgotPassword = function() {
+             if (_.isEmpty(self.data.email)) {
+                 tostService.notify('Email Address Required', 'top');
+             } else {
+                 self.forgotSpinner = true;
+                 var query = forgotPasswordFactory.save({
+                     email: self.data.email
+                 });
+                 query.$promise.then(function(data) {
+                     console.log(data);
+                     self.forgotSpinner = false;
+                     tostService.notify(data.message, 'top');
+                     if (data.status == 1) {
+                         $state.go('login');
+                     }
+                 });
+             }
+         };
+     }
+ })();
--- design/www/app/forgotpassword/forgotpassword.factory.js
+++ design/www/app/forgotpassword/forgotpassword.factory.js
@@ -1,9 +1,9 @@
 (function() {
-   'use strict';
-   angular.module('starter')
-       .factory('forgotPasswordFactory', forgotPasswordFactory);
+    'use strict';
+    angular.module('starter')
+        .factory('forgotPasswordFactory', forgotPasswordFactory);
 
-   function forgotPasswordFactory($resource, Configurations) {
-       return $resource(Configurations.api_url+'/users/forgot_password/:email', {},{});
-   };
+    function forgotPasswordFactory($resource, Configurations) {
+        return $resource(Configurations.api_url + '/users/forgot_password/:email', {}, {});
+    };
 })();
