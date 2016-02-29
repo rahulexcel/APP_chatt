5e109392c3f1b8abe32c36a92dd554b328397b63
rahulexcel <rahul.excel2011@gmail.com>
2016-02-29 18:50:19 +0530

    changes in directive

---
+++ design/www/app/chatpage/chatpageheader.directive.js
@@ -0,0 +1,19 @@
+ (function() {
+     'use strict';
+
+     angular.module('starter')
+
+     .directive('chatPageHeader', function() {
+         var directive = {};
+         directive.restrict = 'E';
+         directive.templateUrl = "app/chatpage/templates/header.html";
+         directive.scope = {
+             chatPage: "=header"
+         }
+         directive.compile = function(element, attributes) {
+             var linkFunction = function($scope, element, attributes) {}
+             return linkFunction;
+         }
+         return directive;
+     });
+ })();
