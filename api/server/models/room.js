eba086b4d5ed88951c66badfb49f4736a8861944
arun <arun@excellencetechnologies.in>
2016-03-09 12:05:06 +0530

    new room with socket

--- api/server/models/room.js
+++ api/server/models/room.js
@@ -1,8 +1,6 @@
 var UTIL = require('../modules/generic');
 var ObjectID = require('mongodb').ObjectID;
 
-var loopback = require('loopback');
-
 module.exports = function (Room) {
     Room.disableRemoteMethod("create", true);
     Room.disableRemoteMethod("upsert", true);
@@ -28,19 +26,17 @@ module.exports = function (Room) {
     Room.disableRemoteMethod('__updateById__accessTokens', false);
     //-------------------------------------------------------------
     
-    
-    
-    //********************************* START REGISTER AND LOGIN **********************************
-    Room.create_private_room = function ( req, chat_with, currentTimestamp, callback) {
-        if( typeof req.accessToken == 'undefined' || req.accessToken == null || req.accessToken == '' || typeof req.accessToken.userId == 'undefined' || req.accessToken.userId == '' ){
-            callback(null, 0, 'UnAuthorized', {});
-        }else{
-            var access_token_userid = req.accessToken.userId;
-            Room.findById( access_token_userid, function(err, user) {
-                if( err ){
-                    callback(null, 0, 'UnAuthorized 1', {});
+    Room.create_private_room = function ( accessToken, chat_with, currentTimestamp, callback) {
+        var User = Room.app.models.User;
+        User.relations.accessTokens.modelTo.findById(accessToken, function(err, accessToken) {
+            if( err ){
+                callback(null, 0, 'UnAuthorized', {});
+            }else{
+                if( !accessToken ){
+                    callback(null, 0, 'UnAuthorized', {});
                 }else{
-                    var owner_user_id = access_token_userid;
+                    
+                    var owner_user_id = accessToken.userId;
                     var room_users = [
                         owner_user_id,
                         chat_with
@@ -88,14 +84,16 @@ module.exports = function (Room) {
                         }
                     });
                 }
-            });
-        }
+            }
+        });
+        
     };
+    
     Room.remoteMethod(
             'create_private_room', {
-                description: 'Create a private chat room',
+                description: 'create private room',
                 accepts: [
-                    {arg: 'req', type: 'object', 'http': {source: 'req'}},
+                    {arg: 'accessToken', type: 'string'}, 
                     {arg: 'chat_with', type: 'string'}, 
                     {arg: 'currentTimestamp', type: 'number'}
                 ],
@@ -109,10 +107,9 @@ module.exports = function (Room) {
                 }
             }
     );
-    //********************************* END REGISTER AND LOGIN ************************************   
-    
-    //********************************* Start create_room_message ************************************   
-    Room.create_room_message = function ( accessToken, room_id, message, currentTimestamp, callback) {
+    ///////////////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////////////
+    Room.room_message = function ( accessToken, room_id, message, currentTimestamp, callback) {
         var User = Room.app.models.User;
         User.relations.accessTokens.modelTo.findById(accessToken, function(err, accessToken) {
             if( err ){
@@ -140,9 +137,7 @@ module.exports = function (Room) {
                                         'type' : 'text',
                                         'body' : message
                                     },
-                                    message_time: currentTimestamp,
-                                    message_date: UTIL.currentDate(currentTimestamp),
-                                    message_date_time: UTIL.currentDateTimeDay(currentTimestamp)
+                                    message_time: currentTimestamp
                                 });
                                 new_message.save( function(err){
                                     if( err ){
@@ -161,7 +156,7 @@ module.exports = function (Room) {
         });
     };
     Room.remoteMethod(
-            'create_room_message', {
+            'room_message', {
                 description: 'Send a message in room',
                 accepts: [
                     {arg: 'accessToken', type: 'string'}, 
@@ -175,10 +170,90 @@ module.exports = function (Room) {
                     {arg: 'data', type: 'array'}
                 ],
                 http: {
-                    verb: 'post', path: '/create_room_message',
+                    verb: 'post', path: '/room_message',
                 }
             }
     );
-    //********************************* END create_room_message ************************************   
+    ///////////////////////////////////////////////////////////////////////////////////
     
+//    //********************************* START REGISTER AND LOGIN **********************************
+//    Room.create_private_room = function ( req, chat_with, currentTimestamp, callback) {
+//        if( typeof req.accessToken == 'undefined' || req.accessToken == null || req.accessToken == '' || typeof req.accessToken.userId == 'undefined' || req.accessToken.userId == '' ){
+//            callback(null, 0, 'UnAuthorized', {});
+//        }else{
+//            var access_token_userid = req.accessToken.userId;
+//            Room.findById( access_token_userid, function(err, user) {
+//                if( err ){
+//                    callback(null, 0, 'UnAuthorized 1', {});
+//                }else{
+//                    var owner_user_id = access_token_userid;
+//                    var room_users = [
+//                        owner_user_id,
+//                        chat_with
+//                    ];
+//                    var ru = room_users;
+//                    for( var k in room_users ){
+//                        room_users[k] = new ObjectID( room_users[k]);
+//                    }
+//                    var check_where = {
+//                        where : {
+//                            room_users : {'all':room_users}
+//                        }
+//                    };
+//                    Room.find( check_where, function (err, result) {
+//                        if( err ){
+//                            callback(null, 0, 'try again', {});
+//                        }else{
+//                            if( result.length > 0 ){
+//                                result = result[0];
+//                                var room_id = result.id;
+//                                var data = {
+//                                    'room_id' : room_id
+//                                };
+//                                callback(null, 1, 'Room already exists', data);
+//                            }else{
+//                                var new_room = new Room({
+//                                    room_owner : new ObjectID( owner_user_id ),
+//                                    room_users : room_users,
+//                                    registration_time: currentTimestamp,
+//                                    registration_date: UTIL.currentDate(currentTimestamp),
+//                                    registration_date_time: UTIL.currentDateTimeDay(currentTimestamp)
+//                                });
+//                                new_room.save( function(err){
+//                                    if( err ){
+//                                        callback(null, 0, 'try again', {});
+//                                    }else{
+//                                        var room_id = new_room.id;
+//                                        var data = {
+//                                            'room_id' : room_id
+//                                        };
+//                                        callback(null, 1, 'Chat Room Created', data);
+//                                    }
+//                                });
+//                            }
+//                        }
+//                    });
+//                }
+//            });
+//        }
+//    };
+//    Room.remoteMethod(
+//            'create_private_room', {
+//                description: 'Create a private chat room',
+//                accepts: [
+//                    {arg: 'req', type: 'object', 'http': {source: 'req'}},
+//                    {arg: 'chat_with', type: 'string'}, 
+//                    {arg: 'currentTimestamp', type: 'number'}
+//                ],
+//                returns: [
+//                    {arg: 'status', type: 'number'},
+//                    {arg: 'message', type: 'string'},
+//                    {arg: 'data', type: 'array'}
+//                ],
+//                http: {
+//                    verb: 'post', path: '/create_private_room',
+//                }
+//            }
+//    );
+//    //********************************* END REGISTER AND LOGIN ************************************   
 };
--- api/server/sockets.js
+++ api/server/sockets.js
@@ -7,12 +7,33 @@ module.exports.listen = function(app){
     io = socketio.listen(app);
     io.on('connection', function(socket){
         console.log('a user connected');
-        socket.on('create_room_message', function( accessToken, room_id, message, currentTimestamp ){
+        
+        socket.on( 'create_private_room', function( accessToken, chat_with, currentTimestamp ){
+            Room.create_private_room( accessToken, chat_with, currentTimestamp, function( ignore_param, res_status, res_message, res_data ){
+                var response = {
+                    'status' : res_status,
+                    'message' : res_message,
+                    'data' : res_data
+                };
+                socket.emit( 'new_private_room', response );
+            })
+        });    
+        
+        socket.on('room_message', function( accessToken, room_id, message, currentTimestamp ){
+            socket.join( room_id );
             console.log('message: ' + message);
-            Room.create_room_message( accessToken, room_id, message, currentTimestamp, function( ignore_param, res_status, res_message, res_data ){
+            Room.room_message( accessToken, room_id, message, currentTimestamp, function( ignore_param, res_status, res_message, res_data ){
                 console.log( res_status );
                 console.log( res_message );
                 console.log( res_data );
+                if( res_status == 1 ){
+                    var data = {
+                        'name' : 'msg_from_name',
+                        'image' : 'msg_from_image',
+                        'message' : message
+                    };
+                    socket.to( room_id ).emit( 'new_room_message', data );
+                }
             })
         });
     });
