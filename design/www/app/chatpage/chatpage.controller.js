7d285405e2c9c4b3673be210a65dd61b4e73ebec
rahulexcel <rahul.excel2011@gmail.com>
2016-03-10 17:20:42 +0530

    delete consoles.

--- design/www/app/chatpage/chatpage.controller.js
+++ design/www/app/chatpage/chatpage.controller.js
@@ -4,51 +4,49 @@
      angular.module('starter')
          .controller('chatPageController', chatPageController);
 
-     function chatPageController($scope,$state, $timeout, $ionicScrollDelegate, chatPageFactory, $ionicLoading, $ionicHistory, timeStorage, socketService, $stateParams) {
+     function chatPageController($scope, $state, $timeout, $ionicScrollDelegate, chatPageFactory, $ionicLoading, $ionicHistory, timeStorage, socketService, $stateParams) {
          var self = this;
-         // $ionicLoading.show();
-         $scope.userHeader ={};
+         $scope.userHeader = {};
          $scope.userHeader.name = 'kush';
          $scope.userHeader.image = 'http://lorempixel.com/640/480/nature';
          $scope.userHeader.lastSeen = 'LAST SEEN TODAY 08:03 PM';
-         $scope.userHeader.goBack = function(){
+         $scope.userHeader.goBack = function() {
              $ionicHistory.goBack();
          }
          $scope.userfooter = {};
          $scope.userfooter.image = 'http://lorempixel.com/640/480/nature';
-         socket.on('new_room_message',function( data ){
-                       console.log( data );
-                       self.displayChatMessages.push({
-                            "image": data.image,
-                            "message": data.message,
-                            "messageTime":"2:12",
-                            "userName": data.name
-                       });
-                       $scope.$apply();
-                       $ionicScrollDelegate.scrollBottom(false);
-        });
+         socket.on('new_room_message', function(data) {
+             self.displayChatMessages.push({
+                 "image": data.image,
+                 "message": data.message,
+                 "messageTime": "2:12",
+                 "userName": data.name
+             });
+             $scope.$apply();
+             $ionicScrollDelegate.scrollBottom(false);
+         });
          var messageToBeSend = [];
-         $scope.userfooter.sendMessage = function(){
-            socketService.room_message($stateParams.roomId, $scope.userfooter.message);
-            messageToBeSend.push($scope.userfooter.message);
-            timeStorage.set('messageToBeSend',messageToBeSend,1);
-            self.displayChatMessages.push({
-                    "image": "http://lorempixel.com/640/480/nature",
-                    "message":$scope.userfooter.message,
-                    "messageTime":"2:12",
-                    "userName":"User 1"
-            });
-            $scope.userfooter.message = '';
-            $ionicScrollDelegate.scrollBottom(false);
+         $scope.userfooter.sendMessage = function() {
+             socketService.room_message($stateParams.roomId, $scope.userfooter.message);
+             messageToBeSend.push($scope.userfooter.message);
+             timeStorage.set('messageToBeSend', messageToBeSend, 1);
+             self.displayChatMessages.push({
+                 "image": "http://lorempixel.com/640/480/nature",
+                 "message": $scope.userfooter.message,
+                 "messageTime": "2:12",
+                 "userName": "User 1"
+             });
+             $scope.userfooter.message = '';
+             $ionicScrollDelegate.scrollBottom(false);
          }
          $scope.userfooter.inputUp = function() {
-            $timeout(function() {
-              $ionicScrollDelegate.scrollBottom(true);
-            }, 300);
-          };
-          $scope.userfooter.inputDown = function() {
-            $ionicScrollDelegate.resize();
-          };
+             $timeout(function() {
+                 $ionicScrollDelegate.scrollBottom(true);
+             }, 300);
+         };
+         $scope.userfooter.inputDown = function() {
+             $ionicScrollDelegate.resize();
+         };
          var query = chatPageFactory.query({});
          query.$promise.then(function(data) {
              $ionicLoading.hide();
