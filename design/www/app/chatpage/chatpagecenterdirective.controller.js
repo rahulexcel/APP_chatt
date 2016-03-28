bc852f17ef0fcbc27f2deb38bfca589677689f27
rahulexcel <rahul.excel2011@gmail.com>
2016-03-28 18:13:13 +0530

    made service for detect timezone and use

--- design/www/app/chatpage/chatpagecenterdirective.controller.js
+++ design/www/app/chatpage/chatpagecenterdirective.controller.js
@@ -4,9 +4,8 @@
      angular.module('chattapp')
          .controller('chatPageCenterDirectiveController', chatPageCenterDirectiveController);
 
-     function chatPageCenterDirectiveController($scope, $state, $timeout, $ionicScrollDelegate, chatPageFactory, $ionicLoading, $ionicHistory, timeStorage, socketService, $stateParams, sqliteService, chatpageService) {
+     function chatPageCenterDirectiveController($scope, $state, $timeout, $ionicScrollDelegate, chatPageFactory, $ionicLoading, $ionicHistory, timeStorage, socketService, $stateParams, sqliteService, chatpageService, timeZoneService) {
          var self = this;
-         var timeZone = jstz.determine().name();
          var chatWithUserData = timeStorage.get('chatWithUserData');
          var userData = timeStorage.get('userData');
          self.user_id = userData.data.user_id;
@@ -17,7 +16,7 @@
                 self.displayChatMessages.push({
                      "image": response.data.profile_image,
                      "message": response.data.message_body,
-                     "messageTime": moment.unix(response.data.message_time).tz(timeZone).format("hh:mm a"),
+                     "messageTime": moment.unix(response.data.message_time).tz(timeZoneService.getTimeZone()).format("hh:mm a"),
                      "name": response.data.name,
                      "timeStamp": response.data.message_time,
                  });
@@ -30,7 +29,7 @@
                 if(self.displayChatMessages[i].id == response.data.msg_local_id){
                     self.displayChatMessages[i].message_status = 'sent';
                     self.displayChatMessages[i].id = response.data.message_id;
-                    self.displayChatMessages[i].messageTime = moment.unix(response.data.message_time).tz(timeZone).format("hh:mm a");
+                    self.displayChatMessages[i].messageTime = moment.unix(response.data.message_time).tz(timeZoneService.getTimeZone()).format("hh:mm a");
                     self.displayChatMessages[i].timeStamp = response.data.message_time;
                 }
             }
