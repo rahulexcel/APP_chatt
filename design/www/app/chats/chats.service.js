c5a2a7ce6276b5c49d37b90d25073b3bc4505009
deepakexcel <deepak.excel2011@gmail.com>
2016-04-06 16:12:26 +0530

    modification in list room data

--- design/www/app/chats/chats.service.js
+++ design/www/app/chats/chats.service.js
@@ -1,71 +1,57 @@
- (function() {
-     'use strict';
-     angular.module('chattapp')
-         .factory('chatsService', chatsService);
+(function() {
+    'use strict';
+    angular.module('chattapp')
+            .factory('chatsService', chatsService);
 
-     function chatsService($q, timeStorage, chatsFactory, $rootScope, timeZoneService) {
-         var service = {};
-         var q = $q.defer();
-         service.privateRooms = function(roomData, callback) {
-             var userData =  timeStorage.get('userData');
-             var returnData = [];
-             for(var i=0; i < roomData.length; i++){
+    function chatsService($q, timeStorage, chatsFactory, $rootScope, timeZoneService) {
+        var service = {};
+
+        service.privateRooms = function(roomData, callback) {
+            var returnData = [];
+            for (var i = 0; i < roomData.length; i++) {
                 var newRoomData = {};
-                for(var j = 0; j < roomData[i].room_users.length; j++){
-                    if(roomData[i].room_type == 'private'){
-                        roomData[i].room_users[j].last_seenInTimestamp = roomData[i].room_users[j].last_seen;
-                        roomData[i].room_users[j].last_seen = moment(parseInt(roomData[i].room_users[j].last_seen)).format("Do MMMM hh:mm a");
-                        newRoomData.user_data = roomData[i].room_users[j];
-                        newRoomData.user_data.roomType = '';
-                        newRoomData.room_id = roomData[i].id;
-                        returnData.push(newRoomData);
-                        break;
-                    } else if(roomData[i].room_type == 'public'){
-                        var newUserData = {
-                            "id":'',
-                            "last_seen":'',
-                            "last_seenInTimestamp":'',
-                            "name":roomData[i].room_name,
-                            "profile_image":roomData[i].room_image,
-                            "roomType":'(Group)'
-                        }
-                        newRoomData.user_data = newUserData;
-                        newRoomData.room_id = roomData[i].id;
-                        returnData.push(newRoomData);
-                        break;
-                    }
+                if (roomData[i].room_type == "public") {
+                    roomData[i].room_users.last_seen = roomData[i].show_details_for_list.sub_text;
+                } else {
+                    roomData[i].room_users.last_seenInTimestamp = roomData[i].show_details_for_list.sub_text;
+                    roomData[i].room_users.last_seen = moment(parseInt(roomData[i].show_details_for_list.sub_text)).format("Do MMMM hh:mm a");
                 }
-             }
-             if(callback){
+                roomData[i].room_users.profile_image = roomData[i].show_details_for_list.icon;
+                roomData[i].room_users.name = roomData[i].show_details_for_list.main_text;
+                newRoomData.user_data = roomData[i].room_users;
+                newRoomData.user_data.roomType = '';
+                newRoomData.room_id = roomData[i].id;
+                returnData.push(newRoomData);
+            }
+            if (callback) {
                 callback(returnData)
-             }
-             q.resolve(returnData);
-             return q.promise;
-         },
-         service.listMyRooms = function() {
-             var q = $q.defer();
-             var userData =  timeStorage.get('userData');
-             var query = chatsFactory.save({
-                 accessToken: userData.data.access_token,
-                 room_type:'all',
-                 timestamp:_.now(),
-             });
-             query.$promise.then(function(data) {
-                var NoRoomData = [];
-                if(data.data.rooms){
-                    service.privateRooms(data.data.rooms, function(res){
-                        timeStorage.set('displayPrivateChats', res, 1);
-                        $rootScope.$broadcast('updatedRoomData', { data: res });
-                        q.resolve(res);
-                    }); 
-                } else{
-                    timeStorage.set('displayPrivateChats', NoRoomData, 1);
-                    q.resolve(NoRoomData);
-                }               
-             });
-             return q.promise;
-         }
-         return service;
-     };
+            }
+        },
+                service.listMyRooms = function() {
+                    var q = $q.defer();
+                    var userData = timeStorage.get('userData');
+                    var query = chatsFactory.save({
+                        accessToken: userData.data.access_token,
+                        room_type: 'all',
+                        timestamp: _.now(),
+                    });
+                    query.$promise.then(function(data) {
+                        var NoRoomData = [];
+                        if (data.data.rooms) {
+                            service.privateRooms(data.data.rooms, function(res) {
+                                timeStorage.set('displayPrivateChats', res, 1);
+                                $rootScope.$broadcast('updatedRoomData', {data: res});
+                                q.resolve(res);
+                            });
+                        } else {
+                            timeStorage.set('displayPrivateChats', NoRoomData, 1);
+                            q.resolve(NoRoomData);
+                        }
+                    });
+                    return q.promise;
+                }
+        return service;
+    }
+    ;
 
- })();
+})();
