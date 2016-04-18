989852f6d154c64cfbbafda4ac6a03c5437810e4
deepakexcel <deepak.excel2011@gmail.com>
2016-04-18 10:57:00 +0530

    commit

--- design/www/app/common/httpinterceptor.factory.js
+++ design/www/app/common/httpinterceptor.factory.js
@@ -10,12 +10,12 @@
                             var accessToken = currentUser.data.access_token;
                             var configURL = config.url;
 //
-//                            if (configURL.substring(0, 38) == 'http://144.76.34.244:3033/api/uploads/') {
-//                                
-//                                config.url = config.url + '?file_type=profile_image&accessToken=' + accessToken + '&currentTimestamp=' + _.now() + '';
-//
-//                            }
-//                            else
+                            if (configURL.substring(0, 38) == 'http://144.76.34.244:3033/api/uploads/') {
+                                
+                                config.url = config.url + '?file_type=profile_image&accessToken=' + accessToken + '&currentTimestamp=' + _.now() + '';
+
+                            }
+                            else
                             if (configURL.substring(0, 10) == 'http://144') {
 //                                config.url = config.url + '?access_token=' + accessToken + '&currentTimestamp=' + _.now() + '';
                                 config.headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + accessToken};
